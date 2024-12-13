821fe3f259b42b4713d6b8865e71291bbf82f717
nikhat085@gmail.com <nikhat085@gmail.com>
2024-12-13 17:41:37 +0530

    Adding source folder with files along with package.json

---
+++ Source/Debug.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+ 
+void  Debug(void)
+{
+    // This is a single-line comment
+    printf("I'm here in  Debug file. Will use Zypher_Test repo files here\n");
+    //return 0;
+}
---
+++ Source/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void)
+{
+    // This is a single-line comment
+    printf("I'm here in  Simple NodeJS Project\n");
+  
+    return 0;
+}
